b1a27f46a923831baf943952f8803b9922e0a8a1
Mewbi <mewbi.fernandes@gmail.com>
2019-09-08 10:58:29 -0300

    Adicionando funcionalidades

--- LM-CService/CService.sh
+++ LM-CService/CService.sh
@@ -1,37 +1,26 @@
 #!/bin/bash
-#Central de configuração de serviços
 
 #---------------------CABECALHO----------------------------------------------|
-#AUTOR
-#	Gabriel Viana Lourenço <4463gabrielviana@gmil.com>
 #
-#PROGRAMA
-#	Central de Configuração de Serviços
+#  AUTOR
+#     Felipe Fernandes <felipefernandesgsc@gmail.com>
+#	   Gabriel Viana Lourenço <4463gabrielviana@gmil.com>
 #
-#DESCRICAO
-#	Realiza a configuração de serviços de servidores Debian
+#  PROGRAMA
+#	   CService
+#
+#  DATA DE CRIAÇÃO
+#     08/09/2019
+#
+#  DESCRICAO
+#	   Central de acesso aos programas de configuração de serviços
 #
 #----------------------------------------------------------------------------|
 
 #Variavel
-caminho="/home/${usr}/LazyMoon/LM-CService/"
+usr="$1"
+path="/home/${usr}/LazyMoon/LM-CService/"
 
-#Testa conexão com a internet
-clear
-ping www.google.com.br -c 1 >/dev/null;
-	if [ "$?" = "0" ] ;
-		then
-		sleep 2
-	
-	else
-		clear
-		dialog --stdout --title "Falha na Conexão" \
-		   	 	--msgbox "Verifique sua conexão! Não será possivel instalar novos serviços" 0 0
-#		echo -e "Falha na conexão! \nPor favor verifique a saída com a internet para utilização desse programa \n\n\nIremos prosseguir porém não será possivel instalar novos serviços"
-		sleep 4
-		
-	fi
-	
 #Menu de Seleção
 while : ; do
 opcao=$(dialog \
@@ -40,7 +29,7 @@ opcao=$(dialog \
 	0 0 0 \
 	WEB 'APACHE' \
 	DNS 'Bind' \
-	DHCP \
+	DHCP 'ISC DHCP Server' \
 	VOIP 'Asterix' \
 	FTP 'vsFTPd' \
 	Contato 'Entrar em Contato com Suporte' \
@@ -52,29 +41,26 @@ opcao=$(dialog \
 case $opcao in
 
    WEB)
-   bash ${caminho}CS-WEB.sh
+   bash ${path}CS-WEB.sh
    ;;
 
    DNS)
-   bash ${caminho}CS-DNS.sh
+   bash ${path}CS-DNS.sh
    ;;
 
    DHCP)
-   bash ${caminho}CS-DHCP.sh
-   ;;
-
-   VOIP)
-   bash ${caminho}CS-VOIP.sh
+   bash ${path}CS-DHCP.sh
    ;;
 
    FTP)
-   bash ${caminho}CS-FTP.sh
+   bash ${path}CS-FTP.sh
    ;;
 
    Contato)
    dialog \
    --stdout --title "E-mail para contato" \
-   --msgbox "4463gabrielviana@gmail.com" 5 40
+   --msgbox "\nFelipe Fernandes \n felipefernandesgsc@gmail.com \n\n\
+Gabriel Viana \n  4463gabrielviana@gmail.com" 11 40
    ;;
 
    Sair)
